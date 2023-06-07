7266405ec6b0c37f30d9c265bd2b383ef540c908
florentianayuwono <florentianayuwono@gmail.com>
2023-06-07 10:50:34 +0800

    Add code comment

--- post_filter.py
+++ post_filter.py
@@ -123,7 +123,7 @@ def no_at(seq, tail_length=30):
 
 
 def is_chinese_char(cp):
-    """Checks whether CP is the codepoint of a CJK character."""
+    """Checks whether a given Unicode code point `cp` represents a Chinese character."""
     # This defines a "chinese character" as anything in the CJK Unicode block:
     #   https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_(Unicode_block)
     #
@@ -133,14 +133,14 @@ def is_chinese_char(cp):
     # space-separated words, so they are not treated specially and handled
     # like the all of the other languages.
     if (
-            (cp >= 0x4E00 and cp <= 0x9FFF)
-            or (cp >= 0x3400 and cp <= 0x4DBF)  #
-            or (cp >= 0x20000 and cp <= 0x2A6DF)  #
-            or (cp >= 0x2A700 and cp <= 0x2B73F)  #
-            or (cp >= 0x2B740 and cp <= 0x2B81F)  #
-            or (cp >= 0x2B820 and cp <= 0x2CEAF)  #
-            or (cp >= 0xF900 and cp <= 0xFAFF)
-            or (cp >= 0x2F800 and cp <= 0x2FA1F)  #
+            (cp >= 0x4E00 and cp <= 0x9FFF) # represents the Basic Multilingual Plane (BMP) range of CJK Unified Ideographs, which covers most commonly used Chinese characters
+            or (cp >= 0x3400 and cp <= 0x4DBF)  # represents the Extended CJK-A range, which contains additional Chinese characters
+            or (cp >= 0x20000 and cp <= 0x2A6DF)  # covers various supplementary ranges of CJK Unified Ideographs, which include less frequently used characters
+            or (cp >= 0x2A700 and cp <= 0x2B73F)  # - " -
+            or (cp >= 0x2B740 and cp <= 0x2B81F)  # - " -
+            or (cp >= 0x2B820 and cp <= 0x2CEAF)  # - " -
+            or (cp >= 0xF900 and cp <= 0xFAFF) # represents the Compatibility Ideographs range, which contains characters that are used for compatibility with older Chinese character standards
+            or (cp >= 0x2F800 and cp <= 0x2FA1F)  # represents the  Compatibility Ideographs Supplement range, which contains additional compatibility ideographs
     ):  #
         return True
 
